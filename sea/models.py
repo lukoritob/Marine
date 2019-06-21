ce4f80de173f574aabbb8fbcf2498e9ddb6a66fb
Brian Lukorito <50102317+lukoritob@users.noreply.github.com>
2019-06-21 16:39:58 +0300

    Update models.py

--- sea/models.py
+++ sea/models.py
@@ -49,3 +49,69 @@ class Survey_Info(models.Model):
     Site_Name = models.CharField(max_length=20)
     Trans_No = models.IntegerField()
     Date_Survey = models.DateTimeField('date surveyed')
+    
+    #Report models.
+# class Average_fish_density(models.Model):
+#     Name_Local = models.CharField(max_length=30)
+#     AvgOf_Total_Count = models.FloatField()
+
+
+# class Average_fish_density_persite_allyears(models.Model):
+#     SiteName = models.CharField(max_length=20
+#     first = models.CharField(max_length=30)
+#     AvgOf_Total_Count = models.FloatField()
+    
+
+
+# class Annual_average_fish_density_persite(models.Model):
+#     SiteName = models.CharField(max_length=20)
+#     Date_Survey = models.DateTimeField('date surveyed')
+#     Name_Local = models.CharField(max_length=30)
+#     Total_Count = models.IntegerField()
+     
+     
+     
+
+# class Average_benthic_cover(models.Model):
+#     Enter_benthic_category = models.CharField(max_length=20)
+#     Avg_Of_Cover = models.FloatField()
+
+
+# class Average_benthic_cover_persite_allyears(models.Model):
+#     SiteName = models.CharField(max_length=20)
+#     Enter_benthic_category = models.CharField(max_length=20)
+#     Avg_Of_Cover = models.FloatField()
+
+
+# class Annual_average_benthic_cover_persite(models.Model):
+#     pass
+
+# class Average_invertebrates_cover(models.Model):
+#     Name_Invert = models.CharField(max_length=20)
+#     AvgOf_No_Invert = models.FloatField()
+
+
+# class Average_invertebrates_cover_persite_allyears(models.Model):
+#     SiteName = models.CharField(max_length=20)
+#     Date_Survey = models.DateTimeField('date surveyed')
+#     FirstOf_Name_Invert = models.CharField(max_length=30)
+#     SumOf_No_Invert = models.IntegerField()
+#     AvgOf_No_Invert = models.FloatField()
+
+
+# class Annual_average_invertebrate_cover_persite(models.Model):
+#     SiteName = models.CharField(max_length=20)
+#     Date_Survey = models.DateTimeField('date surveyed')
+#     FirstOf_Name_Invert = models.CharField(max_length=30)
+#     AvgOf_No_Invert = models.FloatField()
+    
+
+#overall fish records with averages per fish type per site.
+# class Overall_fishrecords(models.Model):
+#     pass
+
+#overall invertebrate data with % per year per invert per site.
+# class Overall_invertebrate_data(models.Model):
+#     pass
+
+
